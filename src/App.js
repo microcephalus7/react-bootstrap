e8e777f8d41ccf804be84cdfc3eeb3cc9e259fa3
허석호 <microcephalus7@gmail.com>
2020-10-08 16:34:50 +0900

    styled-components 적용

--- src/App.js
+++ src/App.js
@@ -1,85 +1,92 @@
 import React from "react";
+import styled from "styled-components";
+
+const AppWrapper = styled.div`
+  color: black;
+`;
 
 const App = () => {
   return (
-    <div>
+    <AppWrapper>
       <div>
-        <button type="button" class="btn">
-          Basic
-        </button>
-        <button type="button" class="btn btn-primary">
-          Primary
-        </button>
-        <button type="button" class="btn btn-secondary">
-          Secondary
-        </button>
-        <button type="button" class="btn btn-success">
-          Success
-        </button>
-        <button type="button" class="btn btn-info">
-          Info
-        </button>
-        <button type="button" class="btn btn-warning">
-          Warning
-        </button>
-        <button type="button" class="btn btn-danger">
-          Danger
-        </button>
-        <button type="button" class="btn btn-dark">
-          Dark
-        </button>
-        <button type="button" class="btn btn-light">
-          Light
-        </button>
-        <button type="button" class="btn btn-link">
-          Link
-        </button>
-      </div>
-      <div
-        id="carouselExampleIndicators"
-        class="carousel slide"
-        data-ride="carousel"
-      >
-        <ol class="carousel-indicators">
-          <li
-            data-target="#carouselExampleIndicators"
-            data-slide-to="0"
-            class="active"
-          ></li>
-          <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-          <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-        </ol>
-        <div class="carousel-inner">
-          <div class="carousel-item active">
-            <img class="d-block w-100" src="" alt="First slide" />
-          </div>
-          <div class="carousel-item">
-            <img class="d-block w-100" src="" alt="Second slide" />
-          </div>
-          <div class="carousel-item">
-            <img class="d-block w-100" src="" alt="Third slide" />
-          </div>
+        <div>
+          <button type="button" class="btn">
+            Basic
+          </button>
+          <button type="button" class="btn btn-primary">
+            Primary
+          </button>
+          <button type="button" class="btn btn-secondary">
+            Secondary
+          </button>
+          <button type="button" class="btn btn-success">
+            Success
+          </button>
+          <button type="button" class="btn btn-info">
+            Info
+          </button>
+          <button type="button" class="btn btn-warning">
+            Warning
+          </button>
+          <button type="button" class="btn btn-danger">
+            Danger
+          </button>
+          <button type="button" class="btn btn-dark">
+            Dark
+          </button>
+          <button type="button" class="btn btn-light">
+            Light
+          </button>
+          <button type="button" class="btn btn-link">
+            Link
+          </button>
         </div>
-        <a
-          class="carousel-control-prev"
-          href="#carouselExampleIndicators"
-          role="button"
-          data-slide="prev"
+        <div
+          id="carouselExampleIndicators"
+          class="carousel slide"
+          data-ride="carousel"
         >
-          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-          <span class="sr-only">Previous</span>
-        </a>
-        <a
-          class="carousel-control-next"
-          href="#carouselExampleIndicators"
-          role="button"
-          data-slide="next"
-        >
-          <span class="carousel-control-next-icon" aria-hidden="true"></span>
-          <span class="sr-only">Next</span>
-        </a>
+          <ol class="carousel-indicators">
+            <li
+              data-target="#carouselExampleIndicators"
+              data-slide-to="0"
+              class="active"
+            ></li>
+            <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+            <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+          </ol>
+          <div class="carousel-inner">
+            <div class="carousel-item active">
+              <img class="d-block w-100" src="" alt="First slide" />
+            </div>
+            <div class="carousel-item">
+              <img class="d-block w-100" src="" alt="Second slide" />
+            </div>
+            <div class="carousel-item">
+              <img class="d-block w-100" src="" alt="Third slide" />
+            </div>
+          </div>
+          <a
+            class="carousel-control-prev"
+            href="#carouselExampleIndicators"
+            role="button"
+            data-slide="prev"
+          >
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="sr-only">Previous</span>
+          </a>
+          <a
+            class="carousel-control-next"
+            href="#carouselExampleIndicators"
+            role="button"
+            data-slide="next"
+          >
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="sr-only">Next</span>
+          </a>
+        </div>
       </div>
-    </div>
+    </AppWrapper>
   );
 };
 
